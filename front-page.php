4433f4eb4b4b1b1a460f8b6c536a79a1bc47460f
Jason Somai <jsomai458@gmail.com>
2018-04-05 22:40:38 -0400

    started on header and front page

---
+++ front-page.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * Template Name: Front Page Template
+ */
+
+get_header();
+?>
+<div id="masthead" style="background-image:url(<?php echo get_stylesheet_directory_uri(); ?>/1.jpeg);">
+    <div class="container">
+        <div class="row">
+            <div class="col text-right main-text">
+                <h2>From Our Family to Yours</h2>
+    <h1>Welcome Home</h1>
+            </div>
+        </div>
+    </div>
+    <div class="overlay">
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col">
+                    <h3>Our Beliefs and Values</h3>
+                </div>
+                <div class="col">
+                    <h3>Prayer Times</h3>
+                </div>
+            </div>
+        </div>
+    </div>
+    
+</div>
+<?php
+get_footer();
--- header.php
+++ header.php
@@ -22,30 +22,16 @@
 
 <body <?php body_class(); ?>>
 <div id="page" class="site">
-	<a class="skip-link screen-reader-text" href="#content"><?php esc_html_e( 'Skip to content', 'ifc' ); ?></a>
 
-	<header id="masthead" class="site-header">
-		<div class="site-branding">
+	<header>
+        <div class="container-fluid">
+            <div class="row">
+                <div class="site-branding col-2">
 			<?php
-			the_custom_logo();
-			if ( is_front_page() && is_home() ) :
-				?>
-				<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
-				<?php
-			else :
-				?>
-				<p class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></p>
-				<?php
-			endif;
-			$ifc_description = get_bloginfo( 'description', 'display' );
-			if ( $ifc_description || is_customize_preview() ) :
-				?>
-				<p class="site-description"><?php echo $ifc_description; /* WPCS: xss ok. */ ?></p>
-			<?php endif; ?>
+			the_custom_logo();?>IFC  
 		</div><!-- .site-branding -->
-
-		<nav id="site-navigation" class="main-navigation">
-			<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html_e( 'Primary Menu', 'ifc' ); ?></button>
+                <nav id="site-navigation" class="main-navigation col-10 text-right">
+			<button class="menu-toggle d-block d-md-none" aria-controls="primary-menu" aria-expanded="false">=</button>
 			<?php
 			wp_nav_menu( array(
 				'theme_location' => 'menu-1',
@@ -53,6 +39,10 @@
 			) );
 			?>
 		</nav><!-- #site-navigation -->
+            </div>
+        </div>
+		
+
+		
 	</header><!-- #masthead -->
 
-	<div id="content" class="site-content">
