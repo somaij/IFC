28b10b23dc71e36fb314da981053a47904405db6
Jason Somai <jsomai458@gmail.com>
2018-04-29 23:23:59 -0400

    menu dropdown stylez

--- footer.php
+++ footer.php
@@ -81,6 +81,14 @@
                     $('.menu-toggle').removeClass('open');
                 }
             });
+            $("li.menu-item").hover(function() { // mouse enter
+                $(this).find(" > .sub-menu").show(); // display immediate child
+
+            }, function() { // mouse leave
+                if (!$(this).hasClass("current_page_item")) { // check if current page
+                    $(this).find(".sub-menu").hide(); // hide if not current page
+                }
+            });
         });
 
     </script>
