57198b8a631b4ab7279feca21ac9febb11c0518a
Jason Somai <jsomai@Jasons-MacBook-Pro.local>
2019-09-07 17:56:17 -0400

    changed to newest wpgulp

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,3 +1,5 @@
+"use strict";
+
 /**
  * Custom JS.
  *
@@ -5,28 +7,25 @@
  *
  * @since 1.0.0
  */
-console.log( 'CustomJS' );
+console.log('CustomJS');
 
-(function($) {
-    $(document).ready(function() {
-        $("input, textarea").each(function() {
-            if ($(this).val().length != 0) {
-                $(this).parent().siblings("label").addClass("move");
-            }
-            else{
-                $(this).parent().siblings("label").removeClass("move");
-            }
-        
-        });
-        $("input, textarea").focus(function() {
-            $(this).parent().siblings("label").addClass("move");
-        });
-        $("input, textarea").focusout(function() {
-            if ($(this).val().length == 0) {
-                $(this).parent().siblings("label").removeClass("move");
-            }
-        
-        });
-        $('a[data-rel^=lightcase]').lightcase();
+(function ($) {
+  $(document).ready(function () {
+    $("input, textarea").each(function () {
+      if ($(this).val().length != 0) {
+        $(this).parent().siblings("label").addClass("move");
+      } else {
+        $(this).parent().siblings("label").removeClass("move");
+      }
     });
+    $("input, textarea").focus(function () {
+      $(this).parent().siblings("label").addClass("move");
+    });
+    $("input, textarea").focusout(function () {
+      if ($(this).val().length == 0) {
+        $(this).parent().siblings("label").removeClass("move");
+      }
+    });
+    $('a[data-rel^=lightcase]').lightcase();
+  });
 })(jQuery);
